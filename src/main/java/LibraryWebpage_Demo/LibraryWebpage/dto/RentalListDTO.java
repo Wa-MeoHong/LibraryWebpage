10eb75bde3839ef0625bcc10d6da8919a43875de
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2024-01-01 23:32:25 +0900

    RentalListDTO Update and Complete

--- src/main/java/LibraryWebpage_Demo/LibraryWebpage/dto/RentalListDTO.java
+++ src/main/java/LibraryWebpage_Demo/LibraryWebpage/dto/RentalListDTO.java
@@ -1,4 +1,75 @@
+/*
+ *	File : RentalListDTO.java
+ *	Name : Shin Dae Hong
+ *	Data : 2024 / 01 / 01
+ *	objective : Class BookListDTO
+ *			RentalList Table DTO, Used in WEB, Back Server
+ *
+ *	Modified
+ *	=============================================================
+ *		1	|	01/01	|	First Write
+ *		2	|	01/01	|	...
+ *	=============================================================
+ */
+
 package LibraryWebpage_Demo.LibraryWebpage.dto;
 
+
+import LibraryWebpage_Demo.LibraryWebpage.entity.RentalListEntity;
+import lombok.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
 public class RentalListDTO {
+    private Long rentalid;
+    private Long bookid;
+    private Long userid;
+    private String rentalstate;
+    private LocalDate rentaldate;
+    private LocalDate returndate;
+    private int extending;
+
+    // DTO -> Entity Transform Function
+    public RentalListEntity toEntity() {
+        RentalListEntity building = RentalListEntity.builder()
+                .rentalid(rentalid)
+                .bookid(bookid)
+                .userid(userid)
+                .rentalstate(rentalstate)
+                .rentaldate(rentaldate)
+                .returndate(returndate)
+                .extending(extending)
+                .build();
+
+        return building;
+    }
+
+    // Entity -> DTO Transform Function
+    public static RentalListDTO toRentalListDTO(RentalListEntity rentalListEntity){
+        RentalListDTO dto = new RentalListDTO();
+        dto.setRentalid(rentalListEntity.getRentalid());
+        dto.setBookid(rentalListEntity.getBookid());
+        dto.setUserid(rentalListEntity.getUserid());
+        dto.setRentalstate(rentalListEntity.getRentalstate());
+        dto.setRentaldate(rentalListEntity.getRentaldate());
+        dto.setReturndate(rentalListEntity.getReturndate());
+        dto.setExtending(rentalListEntity.getExtending());
+
+        return dto;
+    }
+
+    // Builder를 통한 DTO 생성기
+    @Builder
+    public RentalListDTO(Long rentalid, Long bookid, Long userid, String rentalstate,
+                         LocalDate rentaldate, LocalDate returndate, int extending){
+        this.rentalid = rentalid;
+        this.bookid = bookid;
+        this.userid = userid;
+        this.rentalstate = rentalstate;
+        this.rentaldate = rentaldate;
+        this.returndate = returndate;
+        this.extending = extending;
+    }
+
 }
